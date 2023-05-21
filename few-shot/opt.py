7f39a0d92fbc127e653e267957984db18cc0c55f
Tamanna Hossain-Kay <34294551+atleastbeth3@users.noreply.github.com>
2023-05-21 16:44:40 -0700

    Initial commit

---
+++ few-shot/opt.py
@@ -0,0 +1,163 @@
+import argparse
+from pathlib import Path
+import pandas as pd
+import torch
+import re
+import os
+import sys
+from transformers import AutoTokenizer, OPTForCausalLM
+import random 
+
+gender_codebook=pd.read_csv('pronouns.csv')
+gender_codebook.columns=['gender_type', 'gender', 'nom', 'acc', 'pos_dep', 'pos_ind', 'ref']
+
+#Parse Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--eval_file', type=Path) 
+parser.add_argument('--output_path', type=Path) 
+parser.add_argument('--model', type=str) 
+parser.add_argument('--gender', type=str) 
+    
+args = parser.parse_args()
+eval_file=args.eval_file
+output_path=args.output_path 
+gend=args.gender
+
+#Load
+MODEL='facebook/'+args.model
+tokenizer = AutoTokenizer.from_pretrained(MODEL, use_fast=False, cache_dir='cache/')
+model = OPTForCausalLM.from_pretrained(MODEL, cache_dir='cache/')
+model.eval()
+model.cuda()
+
+def declare_pronouns(gender, sentence):
+  row=gender_codebook[gender_codebook['gender']==gender]
+  row=row.reset_index()
+  sentence=re.sub('{nom}', row['nom'][0],sentence)
+  sentence=re.sub('{acc}', row['acc'][0],sentence)
+  sentence=re.sub('{pos_dep}', row['pos_dep'][0],sentence)
+  sentence=re.sub('{pos_ind}', row['pos_ind'][0], sentence)
+  sentence=re.sub('{ref}', row['ref'][0],sentence)
+  return sentence
+
+def get_label(gender, form):
+  row=gender_codebook[gender_codebook['gender']==gender]
+  row=row.reset_index()
+  label=row[form][0]
+  return label
+
+def label_to_cap(sentence):
+  mask_start=sentence.find('{')
+  if sentence[mask_start-2]=='.':
+    return True
+  else:
+    False
+
+def get_eval_instance(sent_piece, gender, form):
+  sentence=declare_pronouns(gender, sent_piece)
+  label=get_label(gender, form)
+  constraints=gender_codebook[form].tolist()
+
+  if label_to_cap(sentence):
+      label=label.capitalize()
+      constraints=[c.capitalize() for c in constraints]
+  return sentence, label, constraints
+
+def get_context(eval_path, form, template_id, name, gender):
+  eval=pd.read_csv(eval_path)
+  template_ids=list(range(0,9))
+  template_ids=[t for t in template_ids if t!= template_id]
+  #temp_samp=random.sample(template_ids,2)
+  #temp_samp2=template_ids+template_ids+temp_samp
+  temp_samp=random.sample(template_ids,1)
+  temp_samp2=template_ids+temp_samp
+
+  #print(len(temp_samp))
+
+  context=''
+  for t in temp_samp:
+    df=eval[(eval.template_id==t) & (eval.name != name) & (eval.form==form)]
+    df=df.sample()
+    df=df.reset_index()
+    #print(df)
+    genders=gender_codebook[gender_codebook['gender']!=gender]['gender'].tolist()
+    gender2=random.sample(genders,1)[0]
+    sent=declare_pronouns(gender2, df['sentence'][0])
+    #print(sent)
+    substitute=gender_codebook[gender_codebook['gender']==gender2][form].tolist()[0]
+    if label_to_cap(sent):
+     substitute=substitute.capitalize()
+    sent=re.sub('{mask_token}',substitute , sent)
+    context=context+sent+' \n '
+
+  return context
+
+
+def evaluate(eval_path, gender):
+  eval=pd.read_csv(eval_path)
+
+  eval['gender']=''
+  eval['label']=''
+  eval['top_1']=''
+  eval['top_2']=''
+  eval['top_3']=''
+  eval['loss_1']=0
+  eval['loss_2']=0
+  eval['loss_3']=0
+  eval['correct']=0
+  
+  #For each instance
+  for idx, instance in eval.iterrows():
+    print (idx)
+    #Get eval instance
+    sent_piece=instance['sentence']
+    form=instance['form']
+    sentence, label, constraints=get_eval_instance(sent_piece, gender, form)
+    context= get_context(eval_file,form, instance['template_id'], instance['name'], gender)
+    sentence=context+sentence
+
+    eval.at[idx, 'sentence']=sentence
+    eval.at[idx, 'label']=label
+
+    #Evaluate
+    losses=[]
+    for c in constraints:
+        choice=re.sub('{mask_token}', c, sentence)
+        input_ids=tokenizer(choice, return_tensors="pt")["input_ids"].to('cuda')
+        out=model(input_ids, labels=input_ids)  
+        loss=out['loss'].detach().item()
+        losses.append(loss)
+      
+    words=pd.DataFrame({'labels': constraints, 'losses':losses})
+    words = words.sort_values('losses', ascending=True, ignore_index=True)
+
+    eval.at[idx, 'top_1']=words.labels[0]
+    eval.at[idx, 'top_2']=words.labels[1]
+    eval.at[idx, 'top_3']=words.labels[3]
+
+    eval.at[idx, 'loss_1']=words.losses[0]
+    eval.at[idx, 'loss_2']=words.losses[1]
+    eval.at[idx, 'loss_3']=words.losses[3]
+
+    if words.labels[0]==label:
+      eval.at[idx, 'correct']=1
+
+  return eval
+
+def main():
+     
+    #Evaluate
+    for gdx, row in gender_codebook.iterrows():
+      gender=row['gender']
+      file_name=gender+'.csv'
+      file_path=os.path.join(output_path,file_name)
+      if gender==gend:
+
+      	print(gender)
+      	eval=evaluate(eval_file, gender)
+    
+      	#Save Raw results
+      	eval.to_csv(file_path)
+    
+if __name__ == '__main__':
+    main() 
---
+++ zero-shot/opt.py
@@ -0,0 +1,127 @@
+import argparse
+from pathlib import Path
+import pandas as pd
+import torch
+import re
+import os
+import sys
+sys.path.append('/extra/ucinlp0/tthossai/package')
+from transformers import AutoTokenizer, OPTForCausalLM
+gender_codebook=pd.read_csv('pronouns.csv')
+gender_codebook.columns=['gender_type', 'gender', 'nom', 'acc', 'pos_dep', 'pos_ind', 'ref']
+
+#Parse Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--eval_file', type=Path) 
+parser.add_argument('--output_path', type=Path) 
+parser.add_argument('--model', type=str) 
+    
+args = parser.parse_args()
+eval_file=args.eval_file
+output_path=args.output_path 
+
+#Load
+MODEL='facebook/'+args.model
+tokenizer = AutoTokenizer.from_pretrained(MODEL, use_fast=False, cache_dir='cache/')
+model = OPTForCausalLM.from_pretrained(MODEL, cache_dir='cache/')
+model.eval()
+model.cuda()
+
+def declare_pronouns(gender, sentence):
+  row=gender_codebook[gender_codebook['gender']==gender]
+  row=row.reset_index()
+  sentence=re.sub('{nom}', row['nom'][0],sentence)
+  sentence=re.sub('{acc}', row['acc'][0],sentence)
+  sentence=re.sub('{pos_dep}', row['pos_dep'][0],sentence)
+  sentence=re.sub('{pos_ind}', row['pos_ind'][0], sentence)
+  sentence=re.sub('{ref}', row['ref'][0],sentence)
+  return sentence
+
+def get_label(gender, form):
+  row=gender_codebook[gender_codebook['gender']==gender]
+  row=row.reset_index()
+  label=row[form][0]
+  return label
+
+def label_to_cap(sentence):
+  mask_start=sentence.find('{')
+  if sentence[mask_start-2]=='.':
+    return True
+  else:
+    False
+
+def get_eval_instance(sent_piece, gender, form):
+  sentence=declare_pronouns(gender, sent_piece)
+  label=get_label(gender, form)
+  constraints=gender_codebook[form].tolist()
+
+  if label_to_cap(sentence):
+      label=label.capitalize()
+      constraints=[c.capitalize() for c in constraints]
+  return sentence, label, constraints
+
+def evaluate(eval_path, gender):
+  eval=pd.read_csv(eval_path)
+
+  eval['gender']=''
+  eval['label']=''
+  eval['top_1']=''
+  eval['top_2']=''
+  eval['top_3']=''
+  eval['loss_1']=0
+  eval['loss_2']=0
+  eval['loss_3']=0
+  eval['correct']=0
+  
+  #For each instance
+  for idx, instance in eval.iterrows():
+    print (idx)
+    #Get eval instance
+    sent_piece=instance['sentence']
+    form=instance['form']
+    sentence, label, constraints=get_eval_instance(sent_piece, gender, form)
+    eval.at[idx, 'sentence']=sentence
+    eval.at[idx, 'label']=label
+
+    #Evaluate
+    losses=[]
+    for c in constraints:
+        choice=re.sub('{mask_token}', c, sentence)
+        input_ids=tokenizer(choice, return_tensors="pt")["input_ids"].to('cuda')
+        out=model(input_ids, labels=input_ids)  
+        loss=out['loss'].detach().item()
+        losses.append(loss)
+      
+    words=pd.DataFrame({'labels': constraints, 'losses':losses})
+    words = words.sort_values('losses', ascending=True, ignore_index=True)
+
+    eval.at[idx, 'top_1']=words.labels[0]
+    eval.at[idx, 'top_2']=words.labels[1]
+    eval.at[idx, 'top_3']=words.labels[2]
+
+    eval.at[idx, 'loss_1']=words.losses[0]
+    eval.at[idx, 'loss_2']=words.losses[1]
+    eval.at[idx, 'loss_3']=words.losses[2]
+
+    if words.labels[0]==label:
+      eval.at[idx, 'correct']=1
+
+  return eval
+
+def main():
+     
+    #Evaluate
+    for gdx, row in gender_codebook.iterrows():
+      gender=row['gender']
+      file_name=gender+'.csv'
+      file_path=os.path.join(output_path,file_name)
+      if not os.path.exists(file_path):
+
+      	print(gender)
+      	eval=evaluate(eval_file, gender)
+    
+      	#Save Raw results
+      	eval.to_csv(file_path)
+    
+if __name__ == '__main__':
+    main() 
